aec71fdfb7602dbcd8973be57eb65ae0e58a5523
Guillaume Bouvier <bougui505@gmail.com>
2012-11-30 14:53:43 +0100

    add chirality checker in getCoordsFromMap.py

--- application/getCoordsFromMap.py
+++ application/getCoordsFromMap.py
@@ -4,6 +4,7 @@
 import SOMTools
 import numpy
 import os
+
 def getCoordFromNeuron(n):
     naa = n.size/9
     eigenVectors = n[:4*naa].reshape(naa,4)
@@ -15,6 +16,21 @@ def getCoordFromNeuron(n):
     coords = numpy.real( numpy.sqrt( numpy.sign(w[:3])*w[:3] ) * numpy.sign(w[:3]) * v[:,:3] )
     return w[:3].real,v[:,:3].real,coords
 
+def getChirality(coord1,coord2,coord3,coord4):
+    v0 = coord2 - coord1
+    v1 = coord3 - coord2
+    v2 = coord4 - coord3
+    chirality = numpy.dot(numpy.cross(v0,v1),v2)/(numpy.linalg.norm(v1))**3
+    return chirality
+
+def getChiralities(coords):
+    zippedCoords = zip(coords[0:],coords[1:],coords[2:],coords[3:])
+    chiralities = []
+    for e in zippedCoords:
+        chirality = getChirality(e[0],e[1],e[2],e[3])
+        chiralities.append(chirality)
+    return chiralities
+
 smap = numpy.load('map_50x50.dat')
 uMatrix = numpy.load('uMatrix.npy')
 outPath, clusterPathMat, grads = SOMTools.minPath(uMatrix, 4000)
@@ -23,7 +39,14 @@ for i in range(len(outPath)):
     w,v,coords = getCoordFromNeuron(smap[outPath[i]])
     if i == 0:
         vref = v
-    v = v*(numpy.atleast_2d(numpy.sign(numpy.dot(vref.T,v).diagonal())))
-    coords = numpy.sqrt(w)*v
+        coordsRef = coords
+    v1 = v*(numpy.atleast_2d(numpy.sign(numpy.dot(vref.T,v).diagonal())))
+    v2 = v
+#    vs = [ v, v*numpy.atleast_2d([-1,1,1]), v*numpy.atleast_2d([1,-1,1]), v*numpy.atleast_2d([1,1,-1]), v*numpy.atleast_2d([-1,-1,1]), v*numpy.atleast_2d([-1,1,-1]), v*numpy.atleast_2d([1,-1,-1]), v*numpy.atleast_2d([-1,-1,-1]) ]
+    coordsN = [ numpy.sqrt(w)*e for e in [v1,v2,-v1,-v2] ]
+    rmsds = [ ((e - coordsRef)**2).sum(axis=1).mean() for e in coordsN ]
+    chiralities = [ numpy.sum(numpy.sign(getChiralities(e))) for e in coordsN ]
+    print chiralities
+    coords = coordsN[numpy.argmax(chiralities)]
     numpy.savetxt('PDB/s_%d.txt'%i, coords)
-    print "%d/%d"%(i,len(outPath))
+    print "%d/%d %d"%(i,len(outPath),numpy.max(chiralities))
