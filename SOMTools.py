dc5cbdb9928646806150d69109c39f5a4fd4b181
Guillaume Bouvier <bougui505@gmail.com>
2013-01-23 10:06:02 +0100

    modified:   SOMTools.py modified:   application/stepAndSlide.py Move detect_local_minima from application/stepAndSlide.py to SOMTools.py

--- SOMTools.py
+++ SOMTools.py
@@ -14,6 +14,8 @@ import scipy.ndimage.measurements
 import sys
 import tarfile
 import os
+import scipy.ndimage.morphology as morphology
+import scipy.ndimage.filters as filters
 
 def readRestraints(fileName='restraintsList'):
  f = open(fileName)
@@ -454,3 +456,37 @@ def uMovie(fileName = 'MapSnapshots.tar'):
   mapFile.close()
   uMatrix = getUmatrix(map)
   plotMat(uMatrix, 'uMatrices/uMat_%0*d.png'%(4,c.next()))
+
+def detect_local_minima(arr):
+    # http://stackoverflow.com/questions/3684484/peak-detection-in-a-2d-array/3689710#3689710
+    """
+    Takes an array and detects the troughs using the local maximum filter.
+    Returns a boolean mask of the troughs (i.e. 1 when
+    the pixel's value is the neighborhood maximum, 0 otherwise)
+    """
+    # define an connected neighborhood
+    # http://www.scipy.org/doc/api_docs/SciPy.ndimage.morphology.html#generate_binary_structure
+    neighborhood = morphology.generate_binary_structure(len(arr.shape),2)
+    # apply the local minimum filter; all locations of minimum value
+    # in their neighborhood are set to 1
+    # http://www.scipy.org/doc/api_docs/SciPy.ndimage.filters.html#minimum_filter
+    local_min = (filters.minimum_filter(arr, footprint=neighborhood)==arr)
+    # local_min is a mask that contains the peaks we are
+    # looking for, but also the background.
+    # In order to isolate the peaks we must remove the background from the mask.
+    #
+    # we create the mask of the background
+    background = (arr==0)
+    #
+    # a little technicality: we must erode the background in order to
+    # successfully subtract it from local_min, otherwise a line will
+    # appear along the background border (artifact of the local minimum filter)
+    # http://www.scipy.org/doc/api_docs/SciPy.ndimage.morphology.html#binary_erosion
+    eroded_background = morphology.binary_erosion(
+        background, structure=neighborhood, border_value=1)
+    #
+    # we obtain the final mask, containing only peaks,
+    # by removing the background from the local_min mask
+    detected_minima = local_min - eroded_background
+    return numpy.where(detected_minima)
+
--- application/stepAndSlide.py
+++ application/stepAndSlide.py
@@ -23,39 +23,6 @@ class StepAndSlide:
             print "Error: Start and stop points are not isoenergetic!"
             sys.exit(0)
 
-    def detect_local_minima(self, arr):
-        # http://stackoverflow.com/questions/3684484/peak-detection-in-a-2d-array/3689710#3689710
-        """
-        Takes an array and detects the troughs using the local maximum filter.
-        Returns a boolean mask of the troughs (i.e. 1 when
-        the pixel's value is the neighborhood maximum, 0 otherwise)
-        """
-        # define an connected neighborhood
-        # http://www.scipy.org/doc/api_docs/SciPy.ndimage.morphology.html#generate_binary_structure
-        neighborhood = morphology.generate_binary_structure(len(arr.shape),2)
-        # apply the local minimum filter; all locations of minimum value
-        # in their neighborhood are set to 1
-        # http://www.scipy.org/doc/api_docs/SciPy.ndimage.filters.html#minimum_filter
-        local_min = (filters.minimum_filter(arr, footprint=neighborhood)==arr)
-        # local_min is a mask that contains the peaks we are
-        # looking for, but also the background.
-        # In order to isolate the peaks we must remove the background from the mask.
-        #
-        # we create the mask of the background
-        background = (arr==0)
-        #
-        # a little technicality: we must erode the background in order to
-        # successfully subtract it from local_min, otherwise a line will
-        # appear along the background border (artifact of the local minimum filter)
-        # http://www.scipy.org/doc/api_docs/SciPy.ndimage.morphology.html#binary_erosion
-        eroded_background = morphology.binary_erosion(
-            background, structure=neighborhood, border_value=1)
-        #
-        # we obtain the final mask, containing only peaks,
-        # by removing the background from the local_min mask
-        detected_minima = local_min - eroded_background
-        return numpy.where(detected_minima)
-
     def findIsoEnergeticPoints(self,point):
         endValue = self.matrix[point[0],point[1]]
         isoEnergeticPoints = numpy.logical_and(self.matrix >= endValue - self.tol, self.matrix <= endValue + self.tol)
