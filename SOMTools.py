4bdcf080f58007f7a316aff65f1dce546b1b12e0
Guillaume Bouvier <bougui505@gmail.com>
2013-01-31 13:50:37 +0100

    modified:   SOMTools.py detect_local_maxima added

--- SOMTools.py
+++ SOMTools.py
@@ -517,3 +517,40 @@ def detect_local_minima(arr, toricMap=False):
         detected_minima = detected_minima[X:2*X,Y:2*Y]
     return numpy.where(detected_minima)
 
+def detect_local_maxima(arr, toricMap=False):
+    # http://stackoverflow.com/questions/3684484/peak-detection-in-a-2d-array/3689710#3689710
+    """
+    Takes an array and detects the troughs using the local maximum filter.
+    Returns a boolean mask of the troughs (i.e. 1 when
+    the pixel's value is the neighborhood maximum, 0 otherwise)
+    """
+    # define an connected neighborhood
+    # http://www.scipy.org/doc/api_docs/SciPy.ndimage.morphology.html#generate_binary_structure
+    if toricMap:
+        X,Y = arr.shape
+        arr = expandMatrix(arr)
+    neighborhood = morphology.generate_binary_structure(len(arr.shape),2)
+    # apply the local minimum filter; all locations of minimum value
+    # in their neighborhood are set to 1
+    # http://www.scipy.org/doc/api_docs/SciPy.ndimage.filters.html#minimum_filter
+    local_max = (filters.maximum_filter(arr, footprint=neighborhood)==arr)
+    # local_min is a mask that contains the peaks we are
+    # looking for, but also the background.
+    # In order to isolate the peaks we must remove the background from the mask.
+    #
+    # we create the mask of the background
+    background = (arr==0)
+    #
+    # a little technicality: we must erode the background in order to
+    # successfully subtract it from local_min, otherwise a line will
+    # appear along the background border (artifact of the local minimum filter)
+    # http://www.scipy.org/doc/api_docs/SciPy.ndimage.morphology.html#binary_erosion
+    eroded_background = morphology.binary_erosion(
+        background, structure=neighborhood, border_value=1)
+    #
+    # we obtain the final mask, containing only peaks,
+    # by removing the background from the local_min mask
+    detected_maxima = local_max - eroded_background
+    if toricMap:
+        detected_maxima = detected_maxima[X:2*X,Y:2*Y]
+    return numpy.where(detected_maxima)
