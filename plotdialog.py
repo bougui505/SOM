adfc091f92b899a670c33803645c6775dc7f0be9
Mathias Ferber <mferber@borma.bis.pasteur.fr>
2015-06-17 16:52:01 +0200

    debug minimum value for slider 1 (bug on certain trajectories)

--- plotdialog.py
+++ plotdialog.py
@@ -48,7 +48,7 @@ class PlotDialog(MPLDialog):
 
         # Sliders for clustering
         resolution = (self.max_value - self.min_value)/1000
-        self.slider = Tkinter.Scale(parent, from_=self.min_value, to=self.max_value, command=self.get_clusters, orient='horizontal', label='Threshold on U-matrix', length=500, resolution=resolution)
+        self.slider = Tkinter.Scale(parent, from_=self.min_value+resolution, to=self.max_value, command=self.get_clusters, orient='horizontal', label='Threshold on U-matrix', length=500, resolution=resolution)
         self.slider.pack(side='top')
         resolution = (self.max_path_value)/100000
         self.slider2 = Tkinter.Scale(parent, from_=0, to=self.max_path_value, command=self.get_basin, orient='horizontal', label='Threshold on U-matrix for basin flooding', length=500, resolution=resolution)
