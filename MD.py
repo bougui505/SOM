53065e449f9a2f213f13da62464f57b06e63a3e0
Guillaume Bouvier <bougui505@gmail.com>
2016-02-04 16:59:46 +0100

    Implementation of a SOM based String Of Swarms method. The MD engine is based on OpenMM.

---
+++ MD.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: UTF8 -*-
+
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr
+# https://research.pasteur.fr/en/member/guillaume-bouvier/
+# 2016-02-03 12:33:21 (UTC+0100)
+
+from __future__ import print_function
+
+from simtk.openmm import app
+import simtk.openmm as mm
+from simtk import unit
+from sys import stdout
+
+class equilibration:
+    """
+    """
+    def __init__(self, filename_pdb, forcefield_name='amber99sb.xml',
+                 water_model_name='tip3p.xml'):
+        self.pdb = app.PDBFile(filename_pdb)
+        self.modeller = app.Modeller(self.pdb.topology, self.pdb.positions)
+        self.forcefield = app.ForceField(forcefield_name, water_model_name)
+        self.system = None
+        self.integrator = None
+        self.platform = None
+        self.simulation = None
+
+    def add_solvent(self, padding=1.0*unit.nanometers,
+                    ionicStrength=0.1*unit.molar):
+        print("Adding solvent...")
+        self.modeller.addSolvent(self.forcefield, padding=padding,
+                                 ionicStrength=ionicStrength)
+        print("done")
+
+    def create_system(self, nonbondedMethod=app.PME,
+                    nonbondedCutoff=1.0*unit.nanometers,
+                    constraints=app.AllBonds, rigidWater=True,
+                    ewaldErrorTolerance=0.0005, temperature=300*unit.kelvin,
+                    collision_rate=1.0/unit.picoseconds,
+                    timestep=2.0*unit.femtoseconds, platform_name='OpenCL'):
+        print("Creating system...")
+        self.system = self.forcefield.createSystem(self.modeller.topology,
+            nonbondedMethod=nonbondedMethod, nonbondedCutoff=nonbondedCutoff,
+            constraints=constraints, rigidWater=rigidWater,
+            ewaldErrorTolerance=ewaldErrorTolerance)
+        self.integrator = mm.LangevinIntegrator(temperature, collision_rate,
+                                                timestep)
+        self.platform = mm.Platform.getPlatformByName(platform_name)
+        self.simulation = app.Simulation(self.modeller.topology, self.system,
+                                        self.integrator, self.platform)
+        self.simulation.context.setPositions(self.modeller.positions)
+        self.simulation.context.setVelocitiesToTemperature(temperature)
+        print("done")
+
+    def minimize(self):
+        print("Minimizing...")
+        self.simulation.minimizeEnergy()
+        print("done")
+
+    def equilibrate(self, number_of_steps=1000000, report_interval=1000,
+                    filename_output_pdb="equilibrated.pdb",
+                    filename_output_log="openmm_equilibration.log"):
+        print("Equilibrating...")
+        self.simulation.reporters.append(app.StateDataReporter(filename_output_log,
+            report_interval, step=True, time=True, potentialEnergy=True,
+            kineticEnergy=True, totalEnergy=True, temperature=True, volume=True,
+            density=True, progress=True, remainingTime=True, speed=True,
+            totalSteps=number_of_steps, separator='\t'))
+        self.simulation.step(number_of_steps)
+        positions = self.simulation.context.getState(getPositions=True).getPositions()
+        app.PDBFile.writeFile(self.simulation.topology, positions,
+                              open(filename_output_pdb, 'w'))
+        print("done")
+
+class production():
+    """
+    """
+    def __init__(self, filename_pdb, forcefield_name='amber99sb.xml',
+                 water_model_name='tip3p.xml'):
+        self.pdb = app.PDBFile(filename_pdb)
+        self.modeller = app.Modeller(self.pdb.topology, self.pdb.positions)
+        self.forcefield = app.ForceField(forcefield_name, water_model_name)
+        self.system = None
+        self.integrator = None
+        self.platform = None
+        self.simulation = None
+
+    def create_system(self, nonbondedMethod=app.PME,
+                    nonbondedCutoff=1.0*unit.nanometers,
+                    constraints=None, rigidWater=True,
+                    ewaldErrorTolerance=0.0005, temperature=300*unit.kelvin,
+                    collision_rate=1.0/unit.picoseconds,
+                    timestep=1.0*unit.femtoseconds, platform_name='OpenCL'):
+        """
+        Same function as in equilibration class except for the following default values:
+        • constraints = None
+        • timestep = 1fs instead of 2fs
+        """
+        print("Creating system...")
+        self.system = self.forcefield.createSystem(self.modeller.topology,
+            nonbondedMethod=nonbondedMethod, nonbondedCutoff=nonbondedCutoff,
+            constraints=constraints, rigidWater=rigidWater,
+            ewaldErrorTolerance=ewaldErrorTolerance)
+        self.integrator = mm.LangevinIntegrator(temperature, collision_rate,
+                                                timestep)
+        self.platform = mm.Platform.getPlatformByName(platform_name)
+        self.simulation = app.Simulation(self.modeller.topology, self.system,
+                                        self.integrator, self.platform)
+        self.simulation.context.setPositions(self.modeller.positions)
+        self.simulation.context.setVelocitiesToTemperature(temperature)
+        print("done")
+
+    def run(self, number_of_steps=1000000, report_interval=100,
+                    filename_output_dcd="trajectory.dcd",
+                    filename_output_log="openmm_production.log"):
+        print("MD production running...")
+        self.simulation.reporters.append(app.DCDReporter(filename_output_dcd,
+                                                         report_interval))
+        self.simulation.reporters.append(app.StateDataReporter(filename_output_log,
+            report_interval, step=True, time=True, potentialEnergy=True,
+            kineticEnergy=True, totalEnergy=True, temperature=True, volume=True,
+            density=True, progress=True, remainingTime=True, speed=True,
+            totalSteps=number_of_steps, separator='\t'))
+        self.simulation.step(number_of_steps)
+        print("done")
---
+++ SOS.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# -*- coding: UTF8 -*-
+
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr
+# https://research.pasteur.fr/en/member/guillaume-bouvier/
+# 2016-02-04 11:11:17 (UTC+0100)
+
+import sys
+sys.path.append("/home/bougui/lib/SOM")
+import SOM
+import MD
+from MDAnalysis import Universe, Timeseries, collection
+import numpy
+
+class SOS:
+    """
+    SOM based implementation of the String Of Swarms method.
+    """
+    def __init__(self, pdb_1=None, pdb_2= None, smap=None, inputmat=None):
+        """
+        args:
+        • pdb_1: filename of the pdb for the starting structure of the path
+        • pdb_2: filename of the pdb for the ending structure of the path
+        • smap: SOM map
+        • inputmat: input matrix that has been used for SOM training
+          (array of phi-psi dihedrals in complex numbers)
+        attributes:
+        • desc1: descriptor for pdb_1
+        • desc2: descriptor for pdb_2
+        • bmu_1: Best Matching Unit for pdb_1
+        • bmu_2: Best Matching Unit for pdb_2
+        """
+        self.pdb_1 = pdb_1
+        self.pdb_2 = pdb_2
+        self.smap = smap
+        if self.pdb_1 is not None and self.smap is not None:
+            self.bmu_1 = self.find_bmu(self.get_dihedrals_from_pdb(self.pdb_1))
+        else:
+            self.bmu_1 = None
+        if self.pdb_2 is not None and self.smap is not None:
+            self.bmu_2 = self.find_bmu(self.get_dihedrals_from_pdb(self.pdb_2))
+        else:
+            self.bmu_2 = None
+        self.inputmat = inputmat
+        if self.smap is not None and self.inputmat is not None:
+            self.som = SOM.SOM(inputmat, smap=self.smap)
+            self.som.graph.unfold_smap()
+            self.som.get_kinetic_communities()
+        else:
+            self.som = None
+
+        self.metric = 'euclidean'
+
+    def get_dihedrals(self, pdb, dcd):
+        print "Computing dihedrals"
+        universe = Universe(pdb, dcd)
+        protein = universe.select_atoms('protein')
+        n_residue = protein.n_residues
+        collection.clear()
+        for i in range(1, n_residue - 1):
+            phi_sel = universe.residues[i].phi_selection()
+            psi_sel = universe.residues[i].psi_selection()
+            collection.addTimeseries(Timeseries.Dihedral(phi_sel))
+            collection.addTimeseries(Timeseries.Dihedral(psi_sel))
+        collection.compute(universe.trajectory)
+        phi_psi_array = collection.data.T
+        descriptors = numpy.asarray(
+                zip(numpy.cos(phi_psi_array[:,0]) + 1j * numpy.sin(phi_psi_array[:,0]),
+                    numpy.cos(phi_psi_array[:,1]) + 1j * numpy.sin(phi_psi_array[:,1]))
+                )
+
+        if self.inputmat is None:
+            self.inputmat = descriptors
+        else:
+            self.inputmat = r_[self.inputmat, descriptors]
+        print "Shape of the dihedral array: %dx%d"%inputmat
+        print "done"
+
+    def get_dihedrals_from_pdb(self, pdb):
+        universe = Universe(pdb)
+        protein = universe.select_atoms('protein')
+        n_residue = protein.n_residues
+        phi_psi_array = []
+        for i in range(1, n_residue -1):
+            phi_sel = universe.residues[i].phi_selection()
+            psi_sel = universe.residues[i].psi_selection()
+            phi_psi_array.append(numpy.deg2rad(phi_sel.dihedral.value())) # weird behaviour: dihedral.value return angle in degree whereas Timeseries.Dihedral returns angle in radians...
+            phi_psi_array.append(numpy.deg2rad(psi_sel.dihedral.value()))
+        phi_psi_array = numpy.asarray(phi_psi_array)
+        descriptors = numpy.asarray(
+                [numpy.cos(phi_psi_array[0]) + 1j * numpy.sin(phi_psi_array[0]),
+                    numpy.cos(phi_psi_array[1]) + 1j * numpy.sin(phi_psi_array[1])]
+                )
+        return descriptors
+
+    def find_bmu(self, v, return_distance = False):
+        """
+            Find the Best Matching Unit for the input vector v
+        """
+        self.is_complex = False
+        if v.dtype == numpy.asarray(numpy.complex(1, 1)).dtype:
+            self.is_complex = True
+        if numpy.ma.isMaskedArray(self.smap):
+            self.smap = self.smap.filled(numpy.inf)
+        X, Y, cardinal = self.smap.shape
+        if not self.is_complex:
+            cdist = scipy.spatial.distance.cdist(numpy.reshape(v, (1, len(v))),
+                                                 numpy.reshape(self.smap, (X * Y, cardinal)), self.metric)
+        else:
+            shape = self.smap.shape
+            neurons = reduce(lambda x, y: x * y, shape[:-1], 1)
+            cdist = numpy.sqrt(( numpy.abs(self.smap.reshape((neurons, shape[-1])) - v[None]) ** 2 ).sum(axis=1))
+        index = cdist.argmin()
+        if not return_distance:
+            return numpy.unravel_index(index, (X, Y))
+        else:
+            return numpy.unravel_index(index, (X, Y)), cdist[0, index]
